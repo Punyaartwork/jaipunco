efc3e26d8856e389aeb777f3e019a60500f5748d
punyaartwork <punyagroupwork@gmail.com>
2019-04-17 06:46:18 +0700

    test comit3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 8
+                    Laravel 9
                 </div>
 
                 <div class="links">
