700bfb4fc082e6d7c9d4a17a6e0ec408161c837c
punyaartwork <punyagroupwork@gmail.com>
2019-04-20 17:51:50 +0700

    set model post

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable=['user_id','tag_id','postName','post','postDraw','postView','postLike','postComment','postShare','postTime','post_ip'];
+    public function tag()
+    {
+        return $this->belongsTo('App\Tag', 'tag_id');
+    }    
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id');
+    }          
 }
