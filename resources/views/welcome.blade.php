f913d3ab01054a915a3fe71805fde1b0495f153e
punyaartwork <punyagroupwork@gmail.com>
2019-05-13 21:18:27 +0700

    push {{ csrf_field() }}

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,6 +87,7 @@ function checkLoginState() {
 </fb:login-button>
 
 <form action="{{url('checkfacebook')}}" method="post" name="frmMain" id="frmMain">
+{{ csrf_field() }}
 	<input type="hidden" id="hdnFbID" name="hdnFbID">
 	<input type="hidden" id="hdnName" name="hdnName">
 	<input type="hidden" id="hdnEmail" name="hdnEmail"> 
