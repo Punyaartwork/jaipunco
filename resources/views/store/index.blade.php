e2c8f1176f5fe3a528f538cd1043ee38729e1cb1
punyaartwork <punyagroupwork@gmail.com>
2019-05-22 21:29:05 +0700

    change detail store

--- resources/views/store/index.blade.php
+++ resources/views/store/index.blade.php
@@ -4,7 +4,7 @@
 @section('headpage')
 
 <a href="/">
-<div style="max-width: 340px; margin: 20px auto; display: block;"><div style="width: 70px; float: left;"><img src="https://image.flaticon.com/icons/svg/1765/1765052.svg" style="float: left; width: 70px;"></div><div style="width: 270px;float: left;"><div style="font-size: 26px; margin: 0px 15px; float: left; font-weight: 600; color: rgb(0, 181, 204);">ทำธรรมะให้น่าอ่าน</div><div style="font-size: 16px; margin: 0px 15px; float: left; font-weight: 600;">เพื่อทำให้ธรรมะเข้าถึงทุกเพศทุกวัยได้ง่ายขึ้นบนโลกอินเทอร์เน็ต</div></div></div>
+<div style="max-width: 340px; margin: 20px auto; display: block;"><div style="width: 70px; float: left;"><img src="https://image.flaticon.com/icons/svg/1765/1765052.svg" style="float: left; width: 70px;"></div><div style="width: 270px;float: left;"><div style="font-size: 26px; margin: 0px 15px; float: left; font-weight: 600; color: rgb(0, 181, 204);">ทำธรรมะให้น่าอ่าน</div><div style="font-size: 16px; margin: 0px 15px; float: left; font-weight: 600;">เพื่อทำให้ธรรมะเข้าถึงทุกเพศทุกวัยได้ง่ายขึ้น ผ่านภาพวาดที่ดูสบายตา</div></div></div>
 </a>
 <div style="
     height: 50px;
