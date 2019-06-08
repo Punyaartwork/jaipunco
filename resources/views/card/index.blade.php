5f910b3d68d408ca1f88ff2165fb54c1ceb35f5a
punyaartwork <punyagroupwork@gmail.com>
2019-06-08 18:31:21 +0700

    push { in card

--- resources/views/card/index.blade.php
+++ resources/views/card/index.blade.php
@@ -356,15 +356,16 @@
                                 for (var i = 0; i < postarray.data.length; i++) { 
                                         if(postarray.data[i].like.length > 0){
                                             for (var j = 0; j <postarray.data[i].like.length; j++) { 
-                                                @if(\Session::has('user_id'))
-                                                if(postarray.data[i].like[j].user_id == {{\Session::get('user_id')}}){
-                                                    postarray.data[i].liked = true;
-                                                }else{
-                                                    postarray.data[i].liked = false;
-                                                }
+                                              @if(\Session::has('user_id'))
+                                                    if(postarray.data[i].like[j].user_id == {{\Session::get('user_id')}}){
+                                                        postarray.data[i].liked = true;
+                                                    }else{
+                                                        postarray.data[i].liked = false;
+                                                    }
                                                 @else
                                                     postarray.data[i].liked = false;                                                
                                                 @endif
+                                            }
                                         }else{
                                             postarray.data[i].liked = false;
                                         }
