c138787567ab152b7dfbaf4614e6d89caf01a536
punyaartwork <punyagroupwork@gmail.com>
2019-06-12 10:46:16 +0700

    check like or unlike

--- resources/views/feed/home.blade.php
+++ resources/views/feed/home.blade.php
@@ -559,6 +559,7 @@
                             this.items[i].index = i;                            
                             if(this.items[i].like.length > 0){
                                 for (var j = 0; j < this.items[i].like.length; j++) { 
+                                    alert(this.items[i].like[j].user_id+'==' + {{\Session::get('user_id')}})
                                     @if(\Session::has('user_id'))
                                     if(this.items[i].like[j].user_id == {{\Session::get('user_id')}}){
                                         this.items[i].liked = true;
