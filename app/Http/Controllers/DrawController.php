5bbb487d93b5d8fd9a897d16e7ed26a3677e8496
punyaartwork <punyagroupwork@gmail.com>
2019-06-12 21:44:27 +0700

    dd open

--- app/Http/Controllers/DrawController.php
+++ app/Http/Controllers/DrawController.php
@@ -104,7 +104,7 @@ class DrawController extends Controller
         //chmod($draw->draw,0777);
         if(\File::exists(public_path($filename))){
             \File::delete(public_path($filename));
-            //dd('The file is writable');            
+            dd('The file is writable');            
         }else{
             dd('File does not exists.'.$draw->draw);
         }
