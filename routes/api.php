2e7af62ae21783ab59e3ee011a23a61ad6542594
punyaartwork <punyagroupwork@gmail.com>
2019-07-28 12:10:39 +0700

    checklogin

--- routes/api.php
+++ routes/api.php
@@ -455,6 +455,6 @@ Route::post('checkfacebook', function(Request $request) {
         $session_user = User::where('email', '=',$request->get('hdnEmail'))->first();       
         $session_user->api_token = \Session::get('api');
         $session_user->save();  
-        return redirect('/logined'); 
+        return $session_user; 
     }
 });
