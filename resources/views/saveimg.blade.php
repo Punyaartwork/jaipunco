83ef6f44b65739ae65b28cd6e75d9b8e712c9a5f
punyaartwork <punyagroupwork@gmail.com>
2019-07-28 18:16:10 +0700

    change is url http://www.facebook.com/sharer.php?u=

--- resources/views/saveimg.blade.php
+++ resources/views/saveimg.blade.php
@@ -69,114 +69,27 @@
       <img src="https://jaipun.com/draw/1560588709.png" />
   </div>
   <div class="box paste"></div>
-  <div id="shareFB">SHARE FACEBOOK</div>
+  <div id='shareFB'></div>
 </div>
 <!-- partial -->
-<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 <script>
     $(document).ready(function() {
 	    html2canvas($("#capture"), {
 	    		allowTaint: true,
 	        onrendered: function(canvas) {
-			    $('.paste').prepend(canvas);
+                $('.paste').prepend(canvas);
                 var dataURL = canvas.toDataURL();
         		console.log(dataURL);
                 $('#shareFB').click(function () {
-                    var data = canvas.toDataURL("image/png");
-                    try {
-                        blob = dataURItoBlob(data);
-                    } catch (e) {
-                        console.log(e);
-                    }
-                    FB.getLoginStatus(function (response) {
-                        console.log(response);
-                        if (response.status === "connected") {
-                            postImageToFacebook(response.authResponse.accessToken, "Canvas to Facebook/Twitter", "image/png", blob, window.location.href);
-                        } else if (response.status === "not_authorized") {
-                            FB.login(function (response) {
-                                postImageToFacebook(response.authResponse.accessToken, "Canvas to Facebook/Twitter", "image/png", blob, window.location.href);
-                            }, {scope: "publish_actions"});
-                        } else {
-                            FB.login(function (response) {
-                                postImageToFacebook(response.authResponse.accessToken, "Canvas to Facebook/Twitter", "image/png", blob, window.location.href);
-                            }, {scope: "publish_actions"});
-                        }
-                    });
+                    window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent(dataURL)+'&t='+encodeURIComponent('Jaipun Card'),'sharer','toolbar=0,status=0,width=626,height=436');
                 });
 	        }
       });
       $("#capture").hide();
-     
 	});
 
-    function dataURItoBlob(dataURI) {
-        var byteString = atob(dataURI.split(',')[1]);
-        var ab = new ArrayBuffer(byteString.length);
-        var ia = new Uint8Array(ab);
-        for (var i = 0; i < byteString.length; i++) {
-            ia[i] = byteString.charCodeAt(i);
-        }
-        return new Blob([ab], {type: 'image/png'});
-    }
 
-    function postImageToFacebook(token, filename, mimeType, imageData, message) {
-        var fd = new FormData();
-        fd.append("access_token", token);
-        fd.append("source", imageData);
-        fd.append("no_story", true);
-
-        // Upload image to facebook without story(post to feed)
-        $.ajax({
-            url: "https://graph.facebook.com/me/photos?access_token=" + token,
-            type: "POST",
-            data: fd,
-            processData: false,
-            contentType: false,
-            cache: false,
-            success: function (data) {
-                console.log("success: ", data);
-
-                // Get image source url
-                FB.api(
-                    "/" + data.id + "?fields=images",
-                    function (response) {
-                        if (response && !response.error) {
-                            //console.log(response.images[0].source);
-
-                            // Create facebook post using image
-                            FB.api(
-                                "/me/feed",
-                                "POST",
-                                {
-                                    "message": "",
-                                    "picture": response.images[0].source,
-                                    "link": window.location.href,
-                                    "name": 'Look at the cute panda!',
-                                    "description": message,
-                                    "privacy": {
-                                        value: 'SELF'
-                                    }
-                                },
-                                function (response) {
-                                    if (response && !response.error) {
-                                        /* handle the result */
-                                        console.log("Posted story to facebook");
-                                        console.log(response);
-                                    }
-                                }
-                            );
-                        }
-                    }
-                );
-            },
-            error: function (shr, status, data) {
-                console.log("error " + data + " Status " + shr.status);
-            },
-            complete: function (data) {
-                //console.log('Post to facebook Complete');
-            }
-        });
-    }
 
 /*
   html2canvas 0.5.0-alpha2 <https://html2canvas.hertzen.com>
