b484227b3d9c7349a041b4ce4e07ee611e6da22d
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2019-10-04 09:27:42 +0700

    add user in Like.pbp

--- app/Like.php
+++ app/Like.php
@@ -9,4 +9,8 @@ class Like extends Model
 {
     use SoftDeletes;
     protected $fillable=['card_id','user_id','likeType'];    
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id');
+    } 
 }
