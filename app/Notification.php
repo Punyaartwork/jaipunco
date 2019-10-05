eae457b26ad232978820e7c654f6c12de88900ee
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2019-10-05 09:16:09 +0700

    edit api

--- app/Notification.php
+++ app/Notification.php
@@ -9,8 +9,12 @@ class Notification extends Model
     protected $fillable=['user_id','item_id','item','itemType','notificationStatus','notificationTime','sender'];  
     public function card()
     {
-        return $this->belongsTo('App\Card', 'item_id');
+        return $this->belongsTo('App\Card', 'item_id')->where('notifications.itemType', '=', 1);
     }      
+    public function boon()
+    {
+        return $this->belongsTo('App\Boon', 'item_id')->where('notifications.itemType', '=', 2);
+    } 
     public function sender()
     {
         return $this->belongsTo('App\User', 'sender');
