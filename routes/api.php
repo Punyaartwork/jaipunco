16e1fff8aad150693ed7599beafaa95fee3e7cf9
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2019-10-06 19:32:50 +0700

    edit api

--- routes/api.php
+++ routes/api.php
@@ -987,11 +987,11 @@ Route::delete('merits/{id}', function($id) {
 });
 
 Route::get('onemerititem/{start}/{end}', function($start,$end) {
-    return Merit::with('user')->where('meritTime','>',$start)->where('meritTime','<=',$end)->orderBy('meritItem','desc')->get();
+    return Merit::with('user')->where('meritTime','>',$start)->where('meritTime','<=',$end)->orderBy('meritItem','desc')->frist();
 });
 
 Route::get('onemeritlike/{start}/{end}', function($start,$end) {
-    return Merit::with('user')->where('meritTime','>',$start)->where('meritTime','<=',$end)->orderBy('meritLike','desc')->get();
+    return Merit::with('user')->where('meritTime','>',$start)->where('meritTime','<=',$end)->orderBy('meritLike','desc')->first();
 });
 /*
 |--------------------------------------------------------------------------
