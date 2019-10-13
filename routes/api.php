d446ed20a6613bf3e94dd336cc6250c2b8c0879b
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2019-10-13 20:31:34 +0700

    edit api admires

--- routes/api.php
+++ routes/api.php
@@ -1244,9 +1244,7 @@ Route::get('admires/{id}', function($id) {
 });
 
 Route::post('admires', function(Request $request) {
-    return  $request->post();
     //return Card::create($request->all);
-    /*
     $user = User::where('api_token',$request->api)->get();
     return  Admires::create([
         'user_id'=> $request->user_id,
@@ -1254,7 +1252,7 @@ Route::post('admires', function(Request $request) {
         'votes'=> 1,
         'admire'=> $request->admire,
         'admireTime'=> time(),
-    ]);*/
+    ]);
 });
 
 Route::put('admires/{id}', function(Request $request, $id) {
