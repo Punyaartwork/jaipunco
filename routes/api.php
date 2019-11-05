dfe3db80d384f61aa45f2fced07fcc0b255c7a2c
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2019-11-05 21:43:36 +0700

    test api

--- routes/api.php
+++ routes/api.php
@@ -149,10 +149,10 @@ Route::get('changeprofile/{profile}/{api}', function($profile,$api) {
 
 Route::get('changedetail/{detail}/{api}', function($detail,$api) {
     $user = User::where('api_token',$api)->get();
-    $upload = User::find($user[0]->id)->first();        
+    $upload = User::find($user[0]->id);        
     $upload->detail = $detail;
     $upload->save();
-    return $upload;
+    return $user;
 });
 
 Route::get('users/{api}', function($api) {
