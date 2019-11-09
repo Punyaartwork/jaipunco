cd348c6a9d1b550e8cdcf30e7feb2a37a90d7568
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2019-11-09 11:52:13 +0700

    test notification api to firebase

--- routes/api.php
+++ routes/api.php
@@ -1419,3 +1419,44 @@ Route::post('uploadprofile/{api}', function(Request $request,$api) {
         return 'error';
     }
 });
+
+
+Route::get('testnotification', function() {
+    define('API_ACCESS_KEY','AIzaSyCu4hAVR2msOjx7B_eyn95y1kkTqfU1yGM');
+    $fcmUrl = 'https://fcm.googleapis.com/fcm/send';
+    $token='235zgagasd634sdgds46436';
+
+        $notification = [
+                'title' =>'title',
+                'body' => 'body of message.',
+                'icon' =>'myIcon', 
+                'sound' => 'mySound'
+            ];
+            $extraNotificationData = ["message" => $notification,"moredata" =>'dd'];
+
+            $fcmNotification = [
+                //'registration_ids' => $tokenList, //multple token array
+                'to'        => $token, //single token
+                'notification' => $notification,
+                'data' => $extraNotificationData
+            ];
+
+            $headers = [
+                'Authorization: key=' . API_ACCESS_KEY,
+                'Content-Type: application/json'
+            ];
+
+
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL,$fcmUrl);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fcmNotification));
+            $result = curl_exec($ch);
+            curl_close($ch);
+
+
+        return $result;
+});
