1d3a5abef9829c8743425aba11fc5597312b501f
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2019-11-09 23:53:21 +0700

    add api fcmtest

--- routes/api.php
+++ routes/api.php
@@ -1418,3 +1418,44 @@ Route::post('uploadprofile/{api}', function(Request $request,$api) {
         return 'error';
     }
 });
+
+use LaravelFCM\Message\OptionsBuilder;
+use LaravelFCM\Message\PayloadDataBuilder;
+use LaravelFCM\Message\PayloadNotificationBuilder;
+use FCM;
+
+Route::get('fcmtest', function() {
+$optionBuilder = new OptionsBuilder();
+$optionBuilder->setTimeToLive(60*20);
+
+$notificationBuilder = new PayloadNotificationBuilder('my title');
+$notificationBuilder->setBody('Hello world')
+				    ->setSound('default');
+
+$dataBuilder = new PayloadDataBuilder();
+$dataBuilder->addData(['a_data' => 'my_data']);
+
+$option = $optionBuilder->build();
+$notification = $notificationBuilder->build();
+$data = $dataBuilder->build();
+
+$token = "eJN2WRYJP4k:APA91bGrjsht9ga_ef4VcK9LA3QoYTbxpLZG5xCdsWZ0b2J4I-pVqraYVrMOOLajVawRQXiMXghTQJRJJbeR8g_TZaBhJQOIE23BDqj7QVCIXuF02EywRBAIFjfU7g18EFh2pdfZByia";
+
+$downstreamResponse = FCM::sendTo($token, $option, $notification, $data);
+
+$downstreamResponse->numberSuccess();
+$downstreamResponse->numberFailure();
+$downstreamResponse->numberModification();
+
+// return Array - you must remove all this tokens in your database
+$downstreamResponse->tokensToDelete();
+
+// return Array (key : oldToken, value : new token - you must change the token in your database)
+$downstreamResponse->tokensToModify();
+
+// return Array - you should try to resend the message to the tokens in the array
+$downstreamResponse->tokensToRetry();
+
+// return Array (key:token, value:error) - in production you should remove from your database the tokens
+$downstreamResponse->tokensWithError();
+});
