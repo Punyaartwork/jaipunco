b92a15ebd003d94498eb644eeabccec90fb528f5
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2019-11-10 10:12:35 +0700

    delete ,200 from string()

--- database/migrations/2019_11_10_525338_add_token_to_users.php
+++ database/migrations/2019_11_10_525338_add_token_to_users.php
@@ -14,7 +14,7 @@ class AddtokenToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('token',200);     
+            $table->string('token');     
         });
     }
 
