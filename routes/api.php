4ab6af6ac5a16613ef53951ef151f75a7eb18a99
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2019-12-17 14:15:45 +0700

    move ->with('user')

--- routes/api.php
+++ routes/api.php
@@ -1318,9 +1318,9 @@ Route::get('groupboon', function() {
     /*return DB::table('boons')
              ->select('*')
              ->groupBy('user_id')->pluck('user_id')->paginate(10);*/
-    return        DB::table('boons')->with('user')
+    return        DB::table('boons')
                  ->select('user_id', DB::raw('max(boonTime) as boonTime'))
-                 ->groupBy('user_id')->orderBy('boonTime','desc')
+                 ->groupBy('user_id')->orderBy('boonTime','desc')->with('user')
                  ->paginate(10);
     //return   Boon::paginate(10)->groupBy('user_id');
 });
