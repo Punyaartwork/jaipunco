ca39843a01ba10ee2999ec3c9ee0411963e7b7bc
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2019-12-23 20:50:43 +0700

    edit name Join to  Room

--- database/migrations/2019_12_23_104213_create_rooms_table.php
+++ database/migrations/2019_12_23_104213_create_rooms_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateJoinsTable extends Migration
+class CreateRoomsTable extends Migration
 {
     /**
      * Run the migrations.
