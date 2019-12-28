5d74d5be06a928aa41d05227571bc49ebdeca49d
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2019-12-28 20:10:44 +0700

    edit Good.php

--- routes/api.php
+++ routes/api.php
@@ -1208,7 +1208,7 @@ Route::get('goods/{id}', function($id) {
 
 Route::get('feedgoods', function() {
     return Good::with('boon')->paginate(10)->map(function ($query) {
-        $query->setRelation('boon', $query->boon->take(10));
+        $query->setRelation('boon', $query->boon->take(5));
         return $query;
     });
 });
