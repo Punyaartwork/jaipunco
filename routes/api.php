6d5d58e5b080f5abbc61ad3ecda1b87c60ef6590
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2020-01-07 06:44:12 +0700

    edit api

--- routes/api.php
+++ routes/api.php
@@ -1897,12 +1897,8 @@ Route::get('photos/{id}', function($id) {
 });
 
 Route::post('photos', function(Request $request) {
-    /*$results = $request->photos;
-    foreach ($results as $result){ 
-        echo $result;
-    }*/
     //return User::create($request->all);
-    return  $request->photos;
+    return  $request->post();
 });
 
 Route::put('photos/{id}', function(Request $request, $id) {
