3f69e1996b84b00a72ed27547d4450f6b1ce8563
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2020-02-11 18:37:31 +0700

    add api gooditem

--- routes/api.php
+++ routes/api.php
@@ -1296,6 +1296,12 @@ Route::get('goods', function() {
     // this will return a JSON structure. This will be cleaned up later.
     return Good::all();
 });
+
+Route::get('gooditem/{id}', function($id) {
+    // If the Content-Type and Accept headers are set to 'application/json', 
+    // this will return a JSON structure. This will be cleaned up later.
+    return Good::where('goodItem', 'LIKE', '%'.$id.'%')->paginate(10);
+});
  
 Route::get('goods/{id}', function($id) {
     return Good::find($id);
