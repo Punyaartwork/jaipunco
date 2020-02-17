5ab680018b1ea63de5692553536aa72a3cb78173
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2020-02-17 13:18:25 +0700

    edit form.php

--- resources/views/form.php
+++ resources/views/form.php
@@ -9,19 +9,20 @@
     padding: 29;
     line-height: 2;
 ">ขอเชิญทุกท่านร่วมสถานปนาแอพนักสร้างบารมี ที่มีระบบบันทึกบุญ บันทึกผลการปฏิบัติธรรม ทำบุญออนไลน์ สังคมนักสร้างบารมี รวมอยู่ในแอพเดียว โดยเพียงแค่คลิกปุ่มด้านล่างนี้</p>
-    <a href="https://jaipun.com/prototypr" style="
+    <a href="https://jaipun.com/prototype" style="
     cursor: pointer;
   position: relative;
   text-align: center;
-  margin: 46px 10px 0 0;
+  padding: 46px 10px;
   display: block;
   width: 195px;
   height: 50px;
   text-decoration: none;
-  font-size: 0.7em;
+  font-size: 18;
   font-weight: 600;
   overflow: hidden;
   margin: auto;
+  color:#fff;
   background: #007eff !important;">ร่วมสถานปนาแอพ</a>
     <img style="-webkit-user-select: none;margin: auto;cursor: zoom-in;" src="https://sv1.picz.in.th/images/2020/02/17/xxQ5rz.png" width="529" height="529">
     
