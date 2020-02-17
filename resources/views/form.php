45e170e4d595318dc4c93c5fb93ca7ec25cba318
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2020-02-17 13:15:28 +0700

    add html สำหรับสำรวจไอเดีย

---
+++ resources/views/form.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<meta name="viewport" content="width=device-width, minimum-scale=0.1">
+<title>ร่วมสถานปนาแอพนักสร้างบารมีกับเราง่าย ๆ คลิก!!</title></head>
+
+<body style="margin: 0px;background: #fff;text-align: center;">
+    <p style="
+    width: 500;
+    padding: 29;
+    line-height: 2;
+">ขอเชิญทุกท่านร่วมสถานปนาแอพนักสร้างบารมี ที่มีระบบบันทึกบุญ บันทึกผลการปฏิบัติธรรม ทำบุญออนไลน์ สังคมนักสร้างบารมี รวมอยู่ในแอพเดียว โดยเพียงแค่คลิกปุ่มด้านล่างนี้</p>
+    <a href="https://jaipun.com/prototypr" style="
+    cursor: pointer;
+  position: relative;
+  text-align: center;
+  margin: 46px 10px 0 0;
+  display: block;
+  width: 195px;
+  height: 50px;
+  text-decoration: none;
+  font-size: 0.7em;
+  font-weight: 600;
+  overflow: hidden;
+  margin: auto;
+  background: #007eff !important;">ร่วมสถานปนาแอพ</a>
+    <img style="-webkit-user-select: none;margin: auto;cursor: zoom-in;" src="https://sv1.picz.in.th/images/2020/02/17/xxQ5rz.png" width="529" height="529">
+    
+    </body></html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/form', function () {
+    return view('welcome');
+});
 
 Route::get('/prototype', function () {
     $card = App\Card::find(1);
