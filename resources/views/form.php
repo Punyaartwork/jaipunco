42fe4fe8fa31f88069e2d2dd34d86f909e53e08e
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2020-02-17 13:29:37 +0700

    edit form.php

--- resources/views/form.php
+++ resources/views/form.php
@@ -3,19 +3,12 @@
 <meta name="viewport" content="width=device-width, minimum-scale=0.1">
 <title>ร่วมสถานปนาแอพนักสร้างบารมีกับเราง่าย ๆ คลิก!!</title></head>
 
-<body style="margin: 0px;background: #50514fed;text-align: center;">
+<body style="margin: 0px;background: #50504f;text-align: center;">
 <img style="-webkit-user-select: none;margin: auto;cursor: zoom-in;" src="https://sv1.picz.in.th/images/2020/02/17/xxe78V.png" width="529" height="529">
-    <p style="
-    margin:auto;
-    width: 500;
-    padding: 29;
-    line-height: 2;
-">ขอเชิญทุกท่านร่วมสถานปนาแอพนักสร้างบารมี ที่มีระบบบันทึกบุญ บันทึกผลการปฏิบัติธรรม ทำบุญออนไลน์ สังคมนักสร้างบารมี รวมอยู่ในแอพเดียว โดยเพียงแค่คลิกปุ่มด้านล่างนี้</p>
-    <a href="https://jaipun.com/prototype" style="
+   <a href="https://jaipun.com/prototype" style="
     cursor: pointer;
   position: relative;
   text-align: center;
-  margin: 46px 10px 0 0;
   display: block;
   width: 195px;
   height: 50px;
@@ -25,6 +18,16 @@
   overflow: hidden;
   margin: auto;
   color:#fff;
+  padding: 20px 0px 0 0;
+  border-radius: 20px;
   background: #007eff !important;">ร่วมสถานปนา!!</a>
+      <p style="
+    margin:auto;
+    width: 500;
+    padding: 29;
+    line-height: 2;
+    color: #fff;
+">ขอเชิญทุกท่านร่วมสถานปนาแอพนักสร้างบารมี ที่มีระบบบันทึกบุญ บันทึกผลการปฏิบัติธรรม ทำบุญออนไลน์ สังคมนักสร้างบารมี รวมอยู่ในแอพเดียว โดยเพียงแค่คลิกปุ่มด้านล่างนี้</p>
+ 
     
     </body></html>
